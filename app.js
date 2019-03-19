6b9ed705f40d8cbc9f773ab7689a35b9487e5026
GummyGod <gummy.pwnn@yahoo.com>
2019-03-19 21:56:54 +0200

    set up graphql and define some basic dummy operations

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
 const express = require('express');
+const graphQlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/graphql', graphQlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Cooking','Sailing', 'Coding all night'];
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql:true,
+}));
 
 app.listen(3000, () => console.log('Server up and running...'));
