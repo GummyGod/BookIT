218250d6c02e34398b236088c5b2dc1dca852518
GummyGod <gummy.pwnn@yahoo.com>
2019-03-24 13:35:30 +0200

    query evts  && exclude metadata

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const graphQlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
 
-const events = [];
+const Event = require('./models/event');
 
 const app = express();
 
@@ -41,24 +41,55 @@ app.use('/graphql', graphQlHttp({
     `),
     rootValue: {
         events: () => {
-            return events;
+            return Event.find().then(events => {
+                return events.map(event => {
+                    return {
+                        ...event._doc, // exclude the metadata from our query
+                        _id: event.id //convert the id to string so graphQL can understand id by using the native method
+                    };
+                });
+            }).catch(err => {
+            console.log(err);
+                throw err;
+            });
         },
         createEvent: (args) => {
-            const event = {
-                _id: Math.random().toString(),
+            //generate evt object
+            const event = new Event({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price, //hack to make sure whatever we get here should be converted to a number
-                date: args.eventInput.date
-            }
-            events.push(event);
-            return event;
+                date: new Date(args.eventInput.date)
+            });
+
+            //save evt to db
+            return event.save()
+            .then((result) => {
+                return {
+                    ...result._doc, // exclude the metadata from our query
+                    id: result._doc._id.toString() //basically how to convert id to a type gql understands 
+                    //without using mongoose .id method
+                };
+            })
+            .catch(err => {
+                console.log(err)
+                throw err;
+            });
         }
     },
     graphiql:true,
 }));
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-hgcuo.mongodb.net/test?retryWrites=true`)
+mongoose
+    .connect(
+        `mongodb+srv://${
+            process.env.MONGO_USER
+        }:${
+            process.env.MONGO_PASSWORD
+        }@cluster0-hgcuo.mongodb.net/${
+                process.env.MONGO_DB
+            }?retryWrites=true`
+        )
     .then(() =>{
         app.listen(3000, () => console.log('Server up and running...'));
     }).catch(err => {
