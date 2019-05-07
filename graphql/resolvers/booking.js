5c9da2c4a2412f4933bc9c36aa8eab19f27bec76
GummyGod <gummy.pwnn@yahoo.com>
2019-05-07 11:49:55 +0300

    protect resolvers if not logged in

--- graphql/resolvers/booking.js
+++ graphql/resolvers/booking.js
@@ -3,7 +3,10 @@ const Event = require('../../models/event');
 const {transformBookings, transformEvent} = require('./merge');
 
 module.exports = {
-    bookings: async () => {
+    bookings: async (args,req) => {
+        if (!req.isAuth) {
+            throw new Error('Unathenticated!');
+        }
         try {
             const bookings = await Booking.find();
             return bookings.map(booking => {
@@ -14,7 +17,10 @@ module.exports = {
         }
     },
 
-    bookEvent: async args => {
+    bookEvent: async (args,req) => {
+        if (!req.isAuth) {
+            throw new Error('Unathenticated!');
+        }
         const fetchedEvent = await Event.findOne({_id: args.eventId });
         const booking = new Booking({
             user: '5ca3a46d5a17062d2819846e',
@@ -23,7 +29,10 @@ module.exports = {
         const result = await booking.save();
         return transformBookings(result);
     },
-    cancelBooking: async args => {
+    cancelBooking: async (args,req) => {
+        if (!req.isAuth) {
+            throw new Error('Unathenticated!');
+        }
         try {
             const booking = await Booking.findById(args.bookingId).populate('event');
             const event = transformEvent(booking.event);
