bb812927c4fc98aa474e8fd5207b2244623b0c13
GummyGod <gummy.pwnn@yahoo.com>
2019-05-20 11:31:59 +0300

    new apollo structure i guess?

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-
+import gql from 'graphql-tag';
+import { Mutation } from 'react-apollo';
 
 import './Auth.css';
-import CreateUser from '../graphql/mutations/create-user/create-user';
 
 class AuthPage extends Component {
     state = {
@@ -19,27 +19,41 @@ class AuthPage extends Component {
         const password = this.state.password
 
         if(email.trim().length === 0 || password.trim().length === 0 ) return;
-        
     }
 
     render() {
         console.log(this.props);
+        const CREATE_USER = gql`
+            mutation createUser($email: String!, $password: String!) {
+                createUser(
+                    userInput: {
+                        email: $email
+                        password: $password
+                    }
+                ) {
+                    _id
+                    email
+                }
+            }
+        `;
         return(
-            <form className="auth-form" onSubmit={this.submitHandler}>
-                <div className="form-control">
-                    <label htmlFor="email">E-Mail</label>
-                    <input name="email" type="email" id="email" onChange={this.handleChange} />
-                </div>
-                <p> {this.state.email}</p>
-                <div className="form-control">
-                    <label htmlFor="password">Password</label>
-                    <input name ="password" type="password" id="password" onChange={this.handleChange} />
-                </div>
-                <div className="form-actions">
-                    <button type="submit"> Submit </button>
-                    <button type="button"> Switch to Signup </button>
-                </div>
-            </form>
+            <Mutation mutation={CREATE_USER} variables={this.state.email}>
+                <form className="auth-form" onSubmit={this.submitHandler}>
+                    <div className="form-control">
+                        <label htmlFor="email">E-Mail</label>
+                        <input name="email" type="email" id="email" onChange={this.handleChange} />
+                    </div>
+                    <p> {this.state.email}</p>
+                    <div className="form-control">
+                        <label htmlFor="password">Password</label>
+                        <input name ="password" type="password" id="password" onChange={this.handleChange} />
+                    </div>
+                    <div className="form-actions">
+                        <button type="submit"> Submit </button>
+                        <button type="button"> Switch to Signup </button>
+                    </div>
+                </form>
+            </Mutation>
         );
     }
 }
