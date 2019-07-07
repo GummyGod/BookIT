eb2d66cb521b9a220ae136da7dfcdea7bcf4d595
GummyGod <gummy.pwnn@yahoo.com>
2019-07-07 23:05:37 +0300

    modal form

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -24,13 +24,28 @@ class EventsPage extends Component {
         return(
             <React.Fragment>
                 {this.state.creating && <Backdrop />}
-                {this.state.creating && <Modal 
-                title="Add Event"
-                canCancel
-                canConfirm
-                onCancel={this.onCancelHandler}
-                onConfirm={this.onConfirmHandler}
-                > <p>Modal Content</p> </Modal>}
+                {
+                    this.state.creating && 
+                    <Modal 
+                    title="Add Event"
+                    canCancel
+                    canConfirm
+                    onCancel={this.onCancelHandler}
+                    onConfirm={this.onConfirmHandler}> 
+                        <form>
+                            <div class="form-control">
+                                <label htmlFor="title"> Title </label>
+                                <input type="text" id="title" />
+                                <label htmlFor="price"> Price </label>
+                                <input type="number" id="price" />
+                                <label htmlFor="date"> Date </label>
+                                <input type="date" id="date" />
+                                <label htmlFor="description"> Description </label>
+                                <textarea id="description" rows="4" />
+                            </div>
+                        </form>
+                    </Modal>
+                }
                 <div className="events-control">
                     <p> Create your own events</p>
                     <button 
